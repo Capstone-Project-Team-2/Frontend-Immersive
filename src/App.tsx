b30acb6af85aa456507e111f918e24d7ca52ae4c
Xpow22 <diskaesfandiary22@gmail.com>
2023-10-01 16:12:48 +0700

    feat: slicing dashboard buyer page

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,24 @@ import LoginAdmin from './page/Admin/loginAdmin';
 import LayoutAdmin from './component/layoutAdmin';
 import DashboardAdmin from './page/Admin/dashboardAdmin';
 import ListBuyer from './page/Admin/listBuyer';
+import LoginBuyer from './page/Buyer/loginBuyer';
+import RegisBuyer from './page/Buyer/regisBuyer';
+import DashboardBuyer from './page/Buyer/dashboardBuyer';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          {/* buyyer */}
+          <Route path="/" element={<DashboardBuyer />}>
+          {/* <Route path="/dashboard-buyer" element={<DashboardBuyer />} /> */}
+
+
+          {/* Admin */}
           <Route path="/login-admin" element={<LoginAdmin />} />
-          <Route path="/" element={<LayoutAdmin />}>
-            <Route path="/dashboard-admin" element={<DashboardAdmin />} />
-            <Route path="/list-buyer" element={<ListBuyer />} />
+          <Route path="/dashboard-admin" element={<DashboardAdmin />} />
+          <Route path="/list-buyer" element={<ListBuyer />} />
           </Route>
         </Routes>
       </BrowserRouter>
