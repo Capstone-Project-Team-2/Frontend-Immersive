4ec4b348ec346262e1bcc99d4be14e4adb765b9a
Xpow22 <diskaesfandiary22@gmail.com>
2023-10-05 17:43:57 +0700

    feat:APIprofile

--- src/auth/yup.tsx
+++ src/auth/yup.tsx
@@ -1,24 +1,43 @@
-import * as yup from 'yup';
+import * as yup from "yup";
 
 export const LoginAuthBuyer = yup.object({
   email: yup
     .string()
-    .required(`Email is required`)
+    .required('Email is required')
     .matches(
-      /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/,
-      'Emails must contain symbols @'
+      /^[A-Za-z0-9.%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$/,
+      "Emails must contain symbols @"
     ),
-  password: yup.string().required('Password is required'),
+  password: yup.string().required("Password is required"),
 });
 
 export const RegisterAuthBuyer = yup.object({
-    name: yup.string().required('Full Name is required'),
-    email: yup
-      .string()
-      .required('Email is required')
-      .matches(
-        /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$/,
-        'Emails must contain symbols @'
-      ),
+  name: yup.string().required("Full Name is required"),
+  email: yup
+    .string()
+    .required('Email is required')
+    .matches(
+      /^[A-Za-z0-9.%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$/,
+      "Emails must contain symbols @"
+    ),
+  password: yup.string().required("Password is required"),
+});
+
+export const ProfilBuyer = yup.object({
+  name: yup.string().required("Full Name is required"),
+  email: yup
+    .string()
+    .required('Email is required')
+    .matches(
+      /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$/,
+      "Emails must contain symbols @"
+    ),
     password: yup.string().required('Password is required'),
-  });
+  phone_number: yup.string().required("Phone Number is required"),
+  // .matches(
+  //   /^(^+62|62|^08)(\d{3,4}-?){2}\d{3,4}$/,
+  //   "Phone Number must contain numbers"
+  // ),
+  address: yup.string().required("Address is required"),
+  photo: yup.mixed().required("Photo is required"),
+});
--- src/component/button.tsx
+++ src/component/button.tsx
@@ -1,20 +1,23 @@
 import { FC } from "react";
 
 interface btnProps {
+  id: string;
+  type?: string;
   label?: string;
   color?: string;
   width?: string;
   height?: string;
   onClick?: React.MouseEventHandler;
-}
+} 
 
-const Button: FC<btnProps> = ({ id, label, color, width, height, onClick }) => {
+const Button: FC<btnProps> = ({ id, label, color, width, height, type, onClick }) => {
   return (
     <>
       <button
         id={id}
         onClick={onClick}
-        className={`text-white ${color} w-${width} h-${height} hover:bg-blue-900 border rounded-md px-4 py-2 font-semibold`}
+        type={type}
+        className={`text-white ${color} w-${width} h-${height} bg-blue-950 hover:bg-blue-900 border rounded-md px-4 py-2 font-semibold`}
       >
         {label}
       </button>
